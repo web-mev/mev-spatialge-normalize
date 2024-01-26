c929b05caf5dd43821e6f6792b777e25a3b34052
Brian Lawney <brianlawney@hsph.harvard.edu>
2024-01-26 16:42:20 -0500

    Handling edge case where barcodes in counts and coords files are not equivalent sets

--- docker/stnormalize.R
+++ docker/stnormalize.R
@@ -62,6 +62,38 @@ setwd(working_dir)
 # STlist expects that the first column is the gene names- so we don't use row.names arg
 rnacounts <- read.table(opt$input_file, sep='\t', header=T, check.names=F)
 
+# Same as for the counts, the expectation is that the coordinates file does not
+# have row.names and instead has the barcodes in the first column. For now, however,
+# we set the row names and then later alter.
+spotcoords <- read.table(opt$coordinates_file, sep='\t', row.names=1, header=T, check.names=T)
+
+# the barcodes in coords dataframe can be a superset of the count matrix columns.
+# For example, if the matrix is filtered for QC, there may be poor quality spots
+# that were filtered out. 
+# The opposite is not the case since we cannot have a barcode without a position.
+barcodes_from_counts <- colnames(rnacounts)[2:dim(rnacounts)[2]] 
+diff_set <- setdiff(barcodes_from_counts, rownames(spotcoords))
+num_invalid_barcodes <- length(diff_set)
+if (num_invalid_barcodes > 0) {
+    max_print <- 5
+    if (num_invalid_barcodes < max_print) {
+        invalid_barcodes <- paste(diff_set[1:num_invalid_barcodes], collapse=', ')
+    } else {
+        invalid_barcodes <- sprintf('%s, and %d others.', paste(diff_set[1:max_print], collapse=', '), num_invalid_barcodes-max_print)
+    }
+    message(sprintf('The set of barcodes in your count matrix must be a subset of those in your coordinates file. Problems include: %s', invalid_barcodes))
+    quit(status=1)
+} else {
+    # the STList constructor below will not accept coordinate files which are a superset of the 
+    # count matrix barcodes. We handle that here:
+    spotcoords <- spotcoords[barcodes_from_counts,]
+
+    # we need the barcodes in the first col, not the row names. We used the rownames for indexing
+    # convenience above, but need to change that now:
+    spotcoords <- cbind(rownames(spotcoords), data.frame(spotcoords, row.names=NULL))
+}
+
+# Now, to avoid any unexpected issues downstream, we need to conver the column names, etc.
 # to preserve the barcodes/column names, we create a dataframe of the original and 'R mutated'
 # names. We then run through everything with the mutated names and finally map back.
 orig_col_names <- colnames(rnacounts)
@@ -72,9 +104,7 @@ colname_mapping = data.frame(
     stringsAsFactors=F)
 colnames(rnacounts) <- proper_names
 
-# Same as for the counts, the expectation is that the coordinates file does not
-# have row.names and instead has the barcodes in the first column
-spotcoords <- read.table(opt$coordinates_file, sep='\t', header=T, check.names=T)
+
 spotcoords[,1] <- make.names(spotcoords[,1]) 
 rownames(spotcoords) <- make.names(rownames(spotcoords))
 
